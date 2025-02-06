1a3c1af4d39b38df6ab2c95387da84c95cecac8f
Sami Ullah <su668981@gmail.com>
2025-02-06 22:49:52 +0000

    New Model added

--- Products/models.py
+++ Products/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 import uuid
+import datetime
 # Create your models here.
 
 
@@ -19,16 +20,49 @@ class Category(BaseModel):
     def __str__(self):
         return f"{self.cat_name}"
 
+class Customer(BaseModel):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    phone = models.CharField(max_length=20)
+    
+    email = models.EmailField(max_length=100)
+    password = models.CharField(max_length=50)
+    profile_image = models.ImageField(upload_to="upload/profile",null=True,blank=True)
+
+
+    def __str__(self):
+        return f"{self.first_name}{self.last_name}"
+
+
+
+
 class ProductDetail(BaseModel):
-    product_name = models.ForeignKey(Category,related_name="cat_pro_name",on_delete=models.CASCADE)
+    product_name = models.CharField(max_length=50)
+    category = models.ForeignKey(Category,on_delete=models.CASCADE,null=True, blank=True)
     product_des = models.TextField()
     prduct_price = models.IntegerField()
-    product_image = models.ImageField(upload_to="media")
+    product_image = models.ImageField(upload_to="upload/products")
 
 
     def __str__(self):
         return f"{self.product_name}"
     
 
+
+class Order(BaseModel):
+    productDetail = models.ForeignKey(ProductDetail,on_delete=models.CASCADE)
+    quanitiy = models.IntegerField()
+    customer = models.ForeignKey(Customer,on_delete=models.CASCADE)
+    address = models.CharField(max_length=100)
+    date = models.DateField(default=datetime.datetime.today)
+    status = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.productDetail.product_name
+
+
+
     
+
+
 
