fbad3a6b1f28532ed3d5b4cc710727252b75b709
Sami Ullah <su668981@gmail.com>
2025-02-25 23:03:46 +0000

    App name added

--- FastFood/settings.py
+++ FastFood/settings.py
@@ -166,23 +166,6 @@ STATICFILES_DIRS = [
 
 
 
-LOGGING = {
-    'version': 1,
-    'disable_existing_loggers': False,
-    'handlers': {
-        'file': {
-            'level': 'DEBUG',
-            'class': 'logging.FileHandler',
-            'filename': 'debug.log',
-        },
-    },
-    'loggers': {
-        'django': {
-            'handlers': ['file'],
-            'level': 'DEBUG',
-            'propagate': True,
-        },
-    },
-}
+
 
 
